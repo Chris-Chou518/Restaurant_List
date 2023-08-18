09d017e03fd7fb6d790d2b4f353516553aaccf99
Chris-Chou518 <ttlyes496@gmail.com>
2023-08-18 16:16:05 +0800

    Render restaurant and add show.hbs

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ const express = require('express')
 const {engine} =require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
+
+
+
+app.engine('.hbs',engine({extname:'.hbs'}))
+app.set('view engine', '.hbs')
 
 app.use(express.static('public'))
-
-app.engine('hbs',engine({extname:'hbs'}))
-app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.redirect('/Restaurants')
 })
 
 app.get('/Restaurants', (req, res) => {
-  res.render('index')
+  res.render('index',{restaurants: restaurants})
 })
 
 app.get('/Restaurant/:id', (req, res) => {
   const id = req.params.id
-  res.send(`read restaurant: ${id}`)
+  const restaurant = restaurants.find((rt) => rt.id.toString() === id)
+  res.render('show',{restaurant})
 })
 
 app.listen(port, () =>{
